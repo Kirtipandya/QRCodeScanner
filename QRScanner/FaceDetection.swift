c432b4939818da87f15e4f99493d32a2bccb0324
Kirtikumar Pandya <kirtikumarpandya@kirtikumars-mbp.fritz.box>
2017-08-01 14:24:20 +0200

    QRCodeScanner and Face detection

--- QRScanner/FaceDetection.swift
+++ QRScanner/FaceDetection.swift
@@ -9,10 +9,10 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+class FaceDetection: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     var video = AVCaptureVideoPreviewLayer()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,13 +27,13 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         } catch {
             print("Error")
         }
-     
+        
         let output = AVCaptureMetadataOutput()
         session.addOutput(output)
         
         output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
         
-        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        output.metadataObjectTypes = [AVMetadataObjectTypeFace]
         
         video = AVCaptureVideoPreviewLayer(session: session)
         video.frame = view.layer.bounds
@@ -44,20 +44,21 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     }
     
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
-        if metadataObjects != nil && metadataObjects.count != 0 {
-            
-            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
-                if object.type == AVMetadataObjectTypeQRCode {
-                   let alert = UIAlertController(title: "QR code", message: object.stringValue, preferredStyle: .alert)
+//        if metadataObjects != nil && metadataObjects.count != 0 {
+        
+//            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
+//                if object.type == AVMetadataObjectTypeFace {
+        print(metadataObjects)
+                    let alert = UIAlertController(title: "Human detection", message: " Human found", preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
-                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: {(nil) in
-                        UIPasteboard.general.string = object.stringValue }))
+//                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: {(nil) in
+//                        UIPasteboard.general.string = object.stringValue }))
                     present(alert, animated: true, completion: nil)
-                }
-            }
-        }
+//                }
+//            }
+//        }
     }
-
-
+ 
+    
 }
 
